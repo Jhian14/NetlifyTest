a3c80bab205f81894584e590b953e046841350fa
GiannPogi <gcmediavillo@gmail.com>
2023-05-27 12:01:53 +0800

    Done

--- frontend/src/views/user/list/index.js
+++ frontend/src/views/user/list/index.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from 'react'
+import { React, useEffect, useState } from 'react'
 import axios from "axios"
 
 const UserList = () => {
@@ -8,21 +8,29 @@ const UserList = () => {
         getUsers()
     }, [])
 
-    const getUsers  = async () => {
+    const getUsers = async () => {
         await axios.get(
-            `api/users/`
+            `/api/users/`
         ).then((value) => {
-            console.log("value", value)
-            //setList(value)
+            console.log("value", value.data)
+            setList(value.data)
         }).catch((e) => {
             console.log("e", e)
         })
     }
-    
+
 
     return (
         <div>
             <h1>User List</h1>
+
+            {list.map((user, i) =>
+                <div key={i}>
+                    <p style={{margin: 0}}>Email: {user.email}</p>
+                    <p style={{margin: 0}}>Usename: {user.full_name}</p>
+                    <br/>
+                </div>
+            )}
         </div>
     )
 }
